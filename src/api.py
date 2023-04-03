a0081245fca6fa889864bb6611e997d198fe386b
Enias Cailliau <enias@steamship.com>
2023-04-03 19:38:40 +0100

    Add usage reporting

--- src/api.py
+++ src/api.py
@@ -1,22 +1,32 @@
-from collections import defaultdict
+import json
 import logging
 from typing import Any, Dict, List, Optional, Type
-import json
+
 import openai
 from pydantic import Field
 from steamship import Steamship, Block, Tag, SteamshipError
 from steamship.data.tags.tag_constants import TagKind, RoleTag
 from steamship.invocable import Config, InvocableResponse, InvocationContext
 from steamship.plugin.generator import Generator
-from steamship.plugin.inputs.raw_block_and_tag_plugin_input import RawBlockAndTagPluginInput
-from steamship.plugin.outputs.raw_block_and_tag_plugin_output import RawBlockAndTagPluginOutput
+from steamship.plugin.inputs.raw_block_and_tag_plugin_input import (
+    RawBlockAndTagPluginInput,
+)
+from steamship.plugin.outputs.plugin_output import (
+    UsageReport,
+    OperationType,
+    OperationUnit,
+)
+from steamship.plugin.outputs.raw_block_and_tag_plugin_output import (
+    RawBlockAndTagPluginOutput,
+)
 from steamship.plugin.request import PluginRequest
 from tenacity import (
     after_log,
     retry,
     retry_if_exception_type,
     stop_after_attempt,
-    before_sleep_log, wait_exponential_jitter,
+    before_sleep_log,
+    wait_exponential_jitter,
 )
 
 
@@ -26,31 +36,57 @@ class GPT4Plugin(Generator):
     """
 
     class GPT4PluginConfig(Config):
-        openai_api_key: str = Field("",
-                                    description="An openAI API key to use. If left default, will use Steamship's API key.")
-        max_tokens: int = Field(256, description="The maximum number of tokens to generate per request. Can be overridden in runtime options.")
-        model: Optional[str] = Field("gpt-4",
-                                     description="The OpenAI model to use. Can be a pre-existing fine-tuned model.")
-        temperature: Optional[float] = Field(0.4,
-                                             description="Controls randomness. Lower values produce higher likelihood / more predictable results; higher values produce more variety. Values between 0-1.")
-        top_p: Optional[int] = Field(1,
-                                     description="Controls the nucleus sampling, where the model considers the results of the tokens with top_p probability mass. Values between 0-1.")
-        presence_penalty: Optional[int] = Field(0,
-                                                description="Control how likely the model will reuse words. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. Number between -2.0 and 2.0.")
-        frequency_penalty: Optional[int] = Field(0,
-                                                 description="Control how likely the model will reuse words. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. Number between -2.0 and 2.0.")
-        moderate_output: bool = Field(True,
-                                      description="Pass the generated output back through OpenAI's moderation endpoint and throw an exception if flagged.")
-        max_retries: int = Field(8, description="Maximum number of retries to make when generating.")
-        request_timeout: Optional[float] = Field(600,
-                                                 description="Timeout for requests to OpenAI completion API. Default is 600 seconds.")
-        n: Optional[int] = Field(1, description="How many completions to generate for each prompt.")
+        openai_api_key: str = Field(
+            "",
+            description="An openAI API key to use. If left default, will use Steamship's API key.",
+        )
+        max_tokens: int = Field(
+            256,
+            description="The maximum number of tokens to generate per request. Can be overridden in runtime options.",
+        )
+        model: Optional[str] = Field(
+            "gpt-4",
+            description="The OpenAI model to use. Can be a pre-existing fine-tuned model.",
+        )
+        temperature: Optional[float] = Field(
+            0.4,
+            description="Controls randomness. Lower values produce higher likelihood / more predictable results; higher values produce more variety. Values between 0-1.",
+        )
+        top_p: Optional[int] = Field(
+            1,
+            description="Controls the nucleus sampling, where the model considers the results of the tokens with top_p probability mass. Values between 0-1.",
+        )
+        presence_penalty: Optional[int] = Field(
+            0,
+            description="Control how likely the model will reuse words. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. Number between -2.0 and 2.0.",
+        )
+        frequency_penalty: Optional[int] = Field(
+            0,
+            description="Control how likely the model will reuse words. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. Number between -2.0 and 2.0.",
+        )
+        moderate_output: bool = Field(
+            True,
+            description="Pass the generated output back through OpenAI's moderation endpoint and throw an exception if flagged.",
+        )
+        max_retries: int = Field(
+            8, description="Maximum number of retries to make when generating."
+        )
+        request_timeout: Optional[float] = Field(
+            600,
+            description="Timeout for requests to OpenAI completion API. Default is 600 seconds.",
+        )
+        n: Optional[int] = Field(
+            1, description="How many completions to generate for each prompt."
+        )
 
-        default_role: str = Field(RoleTag.USER.value, description="The default role to use for a block that does not have a Tag of kind='role'")
+        default_role: str = Field(
+            RoleTag.USER.value,
+            description="The default role to use for a block that does not have a Tag of kind='role'",
+        )
 
-        default_system_prompt: str = Field("", description="System prompt that will be prepended before every request")
-
-
+        default_system_prompt: str = Field(
+            "", description="System prompt that will be prepended before every request"
+        )
 
     @classmethod
     def config_cls(cls) -> Type[Config]:
@@ -59,15 +95,13 @@ class GPT4Plugin(Generator):
     config: GPT4PluginConfig
 
     def __init__(
-            self,
-            client: Steamship = None,
-            config: Dict[str, Any] = None,
-            context: InvocationContext = None,
+        self,
+        client: Steamship = None,
+        config: Dict[str, Any] = None,
+        context: InvocationContext = None,
     ):
         super().__init__(client, config, context)
         openai.api_key = self.config.openai_api_key
-
-
 
     def prepare_message(self, block: Block) -> Dict[str, str]:
         role = None
@@ -78,30 +112,33 @@ class GPT4Plugin(Generator):
         if role is None:
             role = self.config.default_role
 
-        return {
-            "role" : role,
-            "content" : block.text
-        }
-
+        return {"role": role, "content": block.text}
 
     def prepare_messages(self, blocks: List[Block]) -> List[Dict[str, str]]:
         messages = []
         if self.config.default_system_prompt != "":
-            messages.append({
-            "role" : RoleTag.SYSTEM,
-            "content" : self.config.default_system_prompt
-        })
+            messages.append(
+                {"role": RoleTag.SYSTEM, "content": self.config.default_system_prompt}
+            )
         # TODO: remove is_text check here when can handle image etc. input
-        messages.extend([self.prepare_message(block) for block in blocks if block.text is not None and block.text != ""])
+        messages.extend(
+            [
+                self.prepare_message(block)
+                for block in blocks
+                if block.text is not None and block.text != ""
+            ]
+        )
         return messages
 
-    def generate_with_retry(self, user: str, messages: List[Dict[str, str]], options: Dict) -> List[Block]:
+    def generate_with_retry(
+        self, user: str, messages: List[Dict[str, str]], options: Dict
+    ) -> (List[Block], List[UsageReport]):
         """Use tenacity to retry the completion call."""
 
         logging.info(f"Making OpenAI dall-e call on behalf of user with id: {user}")
         """Call the API to generate the next section of text."""
 
-        stopwords = options.get('stop', None) if options is not None else None
+        stopwords = options.get("stop", None) if options is not None else None
 
         @retry(
             reraise=True,
@@ -109,10 +146,10 @@ class GPT4Plugin(Generator):
             wait=wait_exponential_jitter(jitter=5),
             before_sleep=before_sleep_log(logging.root, logging.INFO),
             retry=(
-                    retry_if_exception_type(openai.error.Timeout)
-                    | retry_if_exception_type(openai.error.APIError)
-                    | retry_if_exception_type(openai.error.APIConnectionError)
-                    | retry_if_exception_type(openai.error.RateLimitError)
+                retry_if_exception_type(openai.error.Timeout)
+                | retry_if_exception_type(openai.error.APIError)
+                | retry_if_exception_type(openai.error.APIConnectionError)
+                | retry_if_exception_type(openai.error.RateLimitError)
             ),
             after=after_log(logging.root, logging.INFO),
         )
@@ -129,40 +166,70 @@ class GPT4Plugin(Generator):
             )
 
         openai_result = _generate_with_retry()
-        logging.info("Retry statistics: " + json.dumps(_generate_with_retry.retry.statistics))
+        logging.info(
+            "Retry statistics: " + json.dumps(_generate_with_retry.retry.statistics)
+        )
 
         # Fetch text from responses
-        texts = [choice['message']['content'] for choice in openai_result['choices']]
+        texts = [choice["message"]["content"] for choice in openai_result["choices"]]
 
         # for token usage tracking, we need to include not just the token usage, but also completion id
         # that will allow proper usage aggregration for n > 1 cases
-        completion_id = openai_result['id']
-        usage = openai_result['usage']
-        usage['completion-id'] = completion_id
-        tags = [
-            Tag(kind=TagKind.ROLE, name=RoleTag.ASSISTANT),
-            Tag(kind="token_usage", name="token_usage", value=usage),
+        usage = openai_result["usage"]
+        usage["completion_id"] = openai_result["id"]
+
+        usage_reports = [
+            UsageReport(
+                operation_type=OperationType.RUN,
+                operation_unit=OperationUnit.PROMPT_TOKENS,
+                operation_amount=usage["prompt_tokens"],
+                audit_id=usage["completion_id"],
+            ),
+            UsageReport(
+                operation_type=OperationType.RUN,
+                operation_unit=OperationUnit.SAMPLED_TOKENS,
+                operation_amount=usage["completion_tokens"],
+                audit_id=usage["completion_id"],
+            ),
         ]
 
-        return [Block(text=text, tags=tags) for text in texts]
+        return [
+            Block(
+                text=text,
+                tags=[
+                    Tag(kind=TagKind.ROLE, name=RoleTag.ASSISTANT),
+                ],
+            )
+            for text in texts
+        ], usage_reports
 
     @staticmethod
     def _flagged(messages: List[Dict[str, str]]) -> bool:
-        input_text = "\n\n".join([value for role_dict in messages for value in role_dict.values()])
+        input_text = "\n\n".join(
+            [value for role_dict in messages for value in role_dict.values()]
+        )
         moderation = openai.Moderation.create(input=input_text)
-        return moderation['results'][0]['flagged']
+        return moderation["results"][0]["flagged"]
 
     def run(
-            self, request: PluginRequest[RawBlockAndTagPluginInput]
+        self, request: PluginRequest[RawBlockAndTagPluginInput]
     ) -> InvocableResponse[RawBlockAndTagPluginOutput]:
-        """Run the image generator against all the text, combined """
+        """Run the image generator against all the text, combined"""
 
         self.config.extend_with_dict(request.data.options, overwrite=True)
 
         messages = self.prepare_messages(request.data.blocks)
         if self._flagged(messages):
-            raise SteamshipError("Sorry, this content is flagged as inappropriate by OpenAI.")
+            raise SteamshipError(
+                "Sorry, this content is flagged as inappropriate by OpenAI."
+            )
         user_id = self.context.user_id if self.context is not None else "testing"
-        generated_blocks = self.generate_with_retry(messages=messages, user=user_id, options=request.data.options)
+        generated_blocks, usage_reports = self.generate_with_retry(
+            messages=messages, user=user_id, options=request.data.options
+        )
 
-        return InvocableResponse(data=RawBlockAndTagPluginOutput(blocks=generated_blocks))
+        return InvocableResponse(
+            data=RawBlockAndTagPluginOutput(
+                blocks=generated_blocks, usage=usage_reports
+            )
+        )
--- test/test_unit.py
+++ test/test_unit.py
@@ -1,6 +1,8 @@
 from steamship import Block, Tag, MimeTypes, SteamshipError
 from steamship.data.tags.tag_constants import TagKind, RoleTag
-from steamship.plugin.inputs.raw_block_and_tag_plugin_input import RawBlockAndTagPluginInput
+from steamship.plugin.inputs.raw_block_and_tag_plugin_input import (
+    RawBlockAndTagPluginInput,
+)
 from steamship.plugin.request import PluginRequest
 
 from src.api import GPT4Plugin
@@ -11,45 +13,73 @@ def test_generator():
     gpt4 = GPT4Plugin(config={"n": 4})
 
     blocks = [
-        Block(text="You are an assistant who loves to count", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.SYSTEM)],
-              mime_type=MimeTypes.TXT),
-        Block(text="1 2 3 4", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)], mime_type=MimeTypes.TXT),
+        Block(
+            text="You are an assistant who loves to count",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.SYSTEM)],
+            mime_type=MimeTypes.TXT,
+        ),
+        Block(
+            text="1 2 3 4",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)],
+            mime_type=MimeTypes.TXT,
+        ),
     ]
 
     new_blocks = gpt4.run(PluginRequest(data=RawBlockAndTagPluginInput(blocks=blocks)))
     assert len(new_blocks.data.blocks) == 4
     for block in new_blocks.data.blocks:
-        assert block.text.strip() == "5 6 7 8 9 10"
-        usage_found = False
-        for tag in block.tags:
-            if tag.kind == "token_usage":
-                usage_found = True
-        assert usage_found
+        assert block.text.strip().startswith("5 6 7 8 9 10")
+
+    assert new_blocks.data.usage is not None
+    assert len(new_blocks.data.usage) == 2
 
 
 def test_stopwords():
     gpt4 = GPT4Plugin()
 
     blocks = [
-        Block(text="You are an assistant who loves to count", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.SYSTEM)],
-              mime_type=MimeTypes.TXT),
-        Block(text="1 2 3 4", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)], mime_type=MimeTypes.TXT),
+        Block(
+            text="You are an assistant who loves to count",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.SYSTEM)],
+            mime_type=MimeTypes.TXT,
+        ),
+        Block(
+            text="1 2 3 4",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)],
+            mime_type=MimeTypes.TXT,
+        ),
     ]
 
-    new_blocks = gpt4.run(PluginRequest(data=RawBlockAndTagPluginInput(blocks=blocks, options={'stop': '6'})))
+    new_blocks = gpt4.run(
+        PluginRequest(
+            data=RawBlockAndTagPluginInput(blocks=blocks, options={"stop": "6"})
+        )
+    )
     assert len(new_blocks.data.blocks) == 1
     assert new_blocks.data.blocks[0].text.strip() == "5"
 
 
 def test_default_prompt():
-    gpt4 = GPT4Plugin(config={'default_system_prompt': "You are very silly and are afraid of numbers. When you see "
-                                                       "them you scream: 'YIKES!'"})
+    gpt4 = GPT4Plugin(
+        config={
+            "default_system_prompt": "You are very silly and are afraid of numbers. When you see "
+            "them you scream: 'YIKES!'"
+        }
+    )
 
     blocks = [
-        Block(text="1 2 3 4", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)], mime_type=MimeTypes.TXT),
+        Block(
+            text="1 2 3 4",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)],
+            mime_type=MimeTypes.TXT,
+        ),
     ]
 
-    new_blocks = gpt4.run(PluginRequest(data=RawBlockAndTagPluginInput(blocks=blocks, options={'stop': '6'})))
+    new_blocks = gpt4.run(
+        PluginRequest(
+            data=RawBlockAndTagPluginInput(blocks=blocks, options={"stop": "6"})
+        )
+    )
     assert len(new_blocks.data.blocks) == 1
     assert new_blocks.data.blocks[0].text.strip() == "YIKES!"
 
@@ -58,10 +88,16 @@ def test_flagged_prompt():
     gpt4 = GPT4Plugin()
 
     blocks = [
-        Block(text=" <Insert something super offensive here to run this test>", tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)], mime_type=MimeTypes.TXT),
+        Block(
+            text=" <Insert something super offensive here to run this test>",
+            tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)],
+            mime_type=MimeTypes.TXT,
+        ),
     ]
 
     with pytest.raises(SteamshipError):
-        new_blocks = gpt4.run(PluginRequest(data=RawBlockAndTagPluginInput(blocks=blocks)))
+        new_blocks = gpt4.run(
+            PluginRequest(data=RawBlockAndTagPluginInput(blocks=blocks))
+        )
         assert len(new_blocks.data.blocks) == 1
         assert new_blocks.data.blocks[0].text.strip() == "YIKES!"
