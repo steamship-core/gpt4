f75cc216322511ff8a8e63e6b810df3eae76098a
MikPoik <42643139+MikPoik@users.noreply.github.com>
2024-09-06 17:16:01 +0000

    Update new supported models, remove deprecated

--- src/api.py
+++ src/api.py
@@ -37,15 +37,16 @@ from tenacity import (
 )
 
 VALID_MODELS_FOR_BILLING = [
+    "gpt-4o",
+    "gpt-4o-2024-08-06",
+    "gpt-4o-mini",
     "gpt-4",
-    "gpt-4-0314",
     "gpt-4-0613",
-    "gpt-4-32k",
-    "gpt-4-32k-0613",
+    "gpt-4-turbo",
+    "gpt-4-turbo-preview",
     "gpt-3.5-turbo",
-    "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-16k",
-    "gpt-3.5-turbo-16k-0613",
+    "gpt-3.5-turbo-1106",
+    "gpt-4-0125-preview",
     "gpt-4-1106-preview"
 ]
 
